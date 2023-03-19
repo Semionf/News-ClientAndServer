a41d2460d00d9b1a980153867ae2bde201b6d6c6
Semion <sfurlender@gmail.com>
2023-03-19 19:07:38 +0200

    .

---
+++ news-client/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import { SiteManager } from "./Components/SiteManager/siteManager";
+import { LoginPage } from "./Components/Pages/LoginPage/loginPage";
+
+function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (!isLoading) {
+    if (isAuthenticated) {
+      return (
+        <>
+          <SiteManager className="whole-page" />
+        </>
+      );
+    } else {
+      return <LoginPage />;
+    }
+  } else {
+    <h1>loading</h1>;
+  }
+}
+
+export default App;
